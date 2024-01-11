3ec2d420e3a4ce221a7b06cdfec417a4d8673442
christophe <christophe.work.utp@outlook.com>
2024-01-11 13:52:17 +0100

    debut react

--- 07_Reactjs/demos/demo-bases/src/App.js
+++ 07_Reactjs/demos/demo-bases/src/App.js
@@ -6,17 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Coucou !!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>J'ai réussi !!!!</h1>
       </header>
     </div>
   );
