d94afe762de50a5dd996678d670b429438e28db2
christophe <christophe.work.utp@outlook.com>
2024-01-19 15:56:51 +0100

    debut correction exercie 04

---
+++ 07_Reactjs/exercices/exercice04/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function App() {
+
+   // const [users,setUsers] = useState([])
+    const [user,setUser] = useState({
+        firstname : "",
+        lastname : ""
+    })
+    const [firstname,setFirstname] = useState("")
+    const [lastname,setLastname] = useState("")
+
+
+    const submitForm = (event) => {
+        event.preventDefault()
+        console.log("fonction submit form")
+        console.log(user)
+        console.log(firstname)
+        console.log(lastname)
+        setUser(prevstate => ({firstname : "titi", lastname : "toto"}))
+    }
+
+  return (
+    <>
+    <h1>Exercice 04</h1>
+        <form action="#" onSubmit={submitForm}>
+            <div>
+                <label htmlFor="firstname">Prénom : </label>
+                <input type="text" value={firstname} onInput={(e) => (setFirstname(e.target.value))}/>
+            </div>
+            <br />
+            <div>
+                <label htmlFor="lastname">Nom : </label>
+                <input type="text" value={lastname}  onInput={(e) => (setLastname(e.target.value))}/>
+            </div>
+            <br />
+            <button>Valider</button>
+        </form>
+        <h2>Bonjour {user.firstname} {user.lastname} !!!</h2>
+    </>
+  )
+}
